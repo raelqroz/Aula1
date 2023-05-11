00b60407ddd86ff4b9f338a05447bf562e7272fc
raelqroz <raelqroz@gmail.com>
2023-05-11 19:56:44 -0300

    funcoes, chamadas de funcao dentro de funcao

---
+++ aula_02/functions.js
@@ -0,0 +1,25 @@
+function banana(n1, n2, n3) {
+    let resultado = (n1 + n2) - n3
+    console.log(resultado)
+
+}
+//banana(10, 20, 5)
+
+
+function soma(n1, n2) {
+    let resultado = n1 + n2
+    console.log('Soma:', resultado)
+return resultado
+
+}
+
+//soma(123, 654)
+
+function media(n1, n2) {
+    //let resultado = (n1+n2)/2
+    let resultado = soma(n1, n2)/2
+    console.log(resultado)
+
+}
+
+media(123, 654)
